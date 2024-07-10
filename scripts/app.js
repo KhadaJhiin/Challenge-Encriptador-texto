cdeaf2564c52c56f7888415a3032865bd581f957
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2024-07-09 22:22:21 -0500

    Se agrega funcionalidad encriptar texto

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,43 @@
-let ingreso = document.getElementById(ingresoTexto).value;
+function encriptarTexto(){
+    let textoUsuario = document.getElementById('ingresoTexto').value;
+    let textoEncriptado = "";
+
+    for(let i=0; i<textoUsuario.length; i++){
+        switch(textoUsuario[i]){
+            case 'a':
+                textoEncriptado += 'ai';
+                break;
+            case 'e':
+                textoEncriptado += 'enter';
+                break;
+            case 'i':
+                textoEncriptado += 'imes';
+                break;
+            case 'o':
+                textoEncriptado += 'ober';
+                break;
+            case 'u':
+                textoEncriptado += 'ufat';
+                break;
+            default:
+                textoEncriptado += textoUsuario[i];
+                break;
+        }
+    }
+    console.log(textoEncriptado);
+    document.getElementById('ingresoTexto').value = '';
+    return textoEncriptado;
+}
+
+function insertarTexto(elemento, texto){
+    let elementoHtml = document.querySelector(elemento,texto);
+    elementoHtml.innerHTML = texto;
+    return;
+}
+
+function actualizarContenido(){
+    let nuevoTexto = encriptarTexto();
+    insertarTexto('.main__areaEncriptacion',`<p>${nuevoTexto}</p>`);
+}
+
+
